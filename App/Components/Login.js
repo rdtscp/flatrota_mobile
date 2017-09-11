b6f0b1b8419a2105f7ce70e0ab3059fdc18d6199
Alex Wilson <acwilson96@protonmail.com>
2017-09-11 16:53:18 +0100

    Initial commit.

---
+++ App/Components/Login.js
@@ -0,0 +1,75 @@
+import React, { Component, } from 'react'
+import {
+  Alert,
+  AppRegistry,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  TextInput,
+} from 'react-native'
+
+class Login extends Component {
+
+    static propTypes = {}
+
+    static defaultProps = {}
+
+    constructor(props) {
+      super(props)
+      this.state = {}
+    }
+  
+    authenticate = () => {
+        this.props.authenticate();
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+              <Text>Enter your name</Text>
+              <TextInput
+                style={{
+                  height: 30, 
+                  width:  300 ,
+                  borderWidth: 1,
+                  borderColor: "rgba(0,0,0,0.5)",
+                }}
+                placeholder={'Username'}
+                placeholderTextColor={"rgba(198,198,204,1)"}
+                onChangeText={(text) => {this.setState({text})}}
+                onSubmitEditing={this.authenticate}
+                value={(this.state && this.state.text) || ''}
+              />
+              <Button
+                onPress={this.authenticate}
+                title="OK"
+                color="#841584"
+                accessibilityLabel="Learn more about this purple button"
+              />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+export default Login
