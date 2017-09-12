150b0fb2bb5b3b775309347312c8ba4d2a5c976f
Alex Wilson <acwilson96@protonmail.com>
2017-09-13 00:47:48 +0100

    Structure for showing and hiding modals.

--- App/Components/Item.js
+++ App/Components/Item.js
@@ -1,5 +1,6 @@
 import React, { Component, } from 'react'
 import {
+  Alert,
   Button,
   StyleSheet,
   Text,
@@ -10,10 +11,6 @@ import {
 
 class Item extends Component {
 
-  static propTypes = {}
-
-  static defaultProps = {}
-
   constructor(props) {
       super(props)
       this.state = {
@@ -21,15 +18,25 @@ class Item extends Component {
       }
   }
 
+  toggleModal() {
+      this.setState(previousState => {
+          return { modalVisible: !previousState.modalVisible };
+      });
+  }
+
+  alertTopup() {
+      Alert.alert('alertTopup');
+  }
+
   render() {
       return (
           <View style={{ padding: 20, margin: 5, height: 75, flex: 1, flexDirection: "row", justifyContent: 'center', backgroundColor: "white" }}>
               <View style={{ width: 100, height: 75, paddingRight: 5 }}>
                   <Button
-                      onPress={this.authenticate}
+                      onPress={this.toggleModal.bind(this)}
                       title="Top Up +"
                       color="green"
-                      accessibilityLabel="Learn more about this purple button"
+                      accessibilityLabel="Declare that you have bought another item."
                   />
               </View>
               <View style={{ width: 100, height: 75 }}>
@@ -37,46 +44,63 @@ class Item extends Component {
               </View>
               <View style={{ width: 100, height: 75, paddingLeft: 5 }}>
                   <Button
-                      onPress={this.authenticate}
+                      onPress={this.alertTopup}
                       title="Run Out!"
                       color="red"
                       accessibilityLabel="Learn more about this purple button"
                   />
               </View>
-          <View> 
-            <Modal 
-              animated={true}
-              transparent={false}
-              visible={(this.state && this.state.modalVisible) }>
-              <View style={styles.container}>
-                  <Text>Quantity</Text>
-                  <TextInput
-                    style={{
-                      height: 30, 
-                      width:  50 ,
-                      borderWidth: 1,
-                      borderColor: "rgba(0,0,0,0.5)",
-                    }}
-                    placeholder={''}
-                    placeholderTextColor={"rgba(198,198,204,1)"}
-                    onChangeText={(text) => {this.setState({text})}}
-                    onSubmitEditing={this.authenticate}
-                    value={(this.state && this.state.text) || ''}
-                  />
-                  <Button
-                    onPress={this.authenticate}
-                    title="OK"
-                    color="#841584"
-                    accessibilityLabel="Learn more about this purple button"
-                  />
-              </View>
-            </Modal>
-          </View>
+              <ItemTopup toggle={this.toggleModal.bind(this)} visible={this.state.modalVisible} />
           </View>
       );
   }
 }
 
+class ItemTopup extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    topup() {
+        Alert.alert('Thanks for topping up!');
+        this.props.toggle();
+    }
+
+    render() {
+        console.log('rendering modal')
+        console.log(this.props.visible)
+        return (
+              <Modal
+                transparent={false}
+                visible={this.props.visible}>
+                <View style={styles.container}>
+                    <Text>Quantity</Text>
+                    <TextInput
+                      style={{
+                        height: 30, 
+                        width:  50 ,
+                        borderWidth: 1,
+                        borderColor: "rgba(0,0,0,0.5)",
+                      }}
+                      placeholder={''}
+                      placeholderTextColor={"rgba(198,198,204,1)"}
+                      onChangeText={(text) => {this.setState({text})}}
+                      onSubmitEditing={this.authenticate}
+                      value={(this.state && this.state.text) || ''}
+                    />
+                    <Button
+                      onPress={this.topup.bind(this)}
+                      title="OK"
+                      color="#841584"
+                      accessibilityLabel="Learn more about this purple button"
+                    />
+                </View>
+              </Modal>
+        );
+    }
+
+}
 
 const styles = StyleSheet.create({
   container: {
